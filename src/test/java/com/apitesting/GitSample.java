5ba1c72e30deafb9653d9ac69bd8f889ba88f941
Prakash1000k <prakash1000k@gmail.com>
2025-10-16 11:41:11 +0530

    Teter1 Modified

--- src/test/java/com/apitesting/GitSample.java
+++ src/test/java/com/apitesting/GitSample.java
@@ -10,5 +10,17 @@ public class GitSample {
 	public void gitTask03() {
 		System.out.println("GIT..........");
 	}
+	public void gitTask04() {
+System.out.println("Tester 1 Branch");
+	}
+	public void gitTask05() {
+		System.out.println("Tester 1 Branch");
+
+	}
+	public void gitTask06() {
+		System.out.println("Tester 1 Branch");
+
+	}
+
 
 }
